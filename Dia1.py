08c47b043a90b899453b6b2667023a945c5feb94
AlvaroContrerasM <145747301+AlvaroContrerasM@users.noreply.github.com>
2023-09-22 11:09:19 +0200

    Add files via upload

---
+++ Dia1.py
@@ -0,0 +1,22 @@
+#Que bonito es Python
+print ("hola mundo")
+print ("Me llamo Álvaro")
+
+#Nuestra primera variable
+x = 5
+
+print (x)
+
+#Creamos variable texto
+
+nombre = "Alvaro"
+numero = "8"  
+
+print (nombre)
+
+
+num1 = 2
+num2 = 8
+suma = num1+num2
+resta = num1-num2
+print ("suma=" +str(suma)+ " | resta=" +str(resta))
