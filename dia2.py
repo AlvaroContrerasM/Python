25cebca53862b2e5d5ebb8e56b103e7abe8f111d
AlvaroContrerasM <145747301+AlvaroContrerasM@users.noreply.github.com>
2023-09-25 14:20:56 +0200

    Add files via upload

---
+++ dia2.py
@@ -0,0 +1,23 @@
+#print ("dime tu nombre:")
+#nombre = input ()
+#print ("hola " + nombre)
+
+
+
+
+numero = int(input("dime tu edad: "))
+print(numero >= 18)
+
+numero = int(input("dime un numero: "))
+print(numero >= 0)
+
+numero = int(input("Nota: "))
+print (numero >= 5 and not numero > 10)
+
+dia = input("Introduce un dia de la semana: ")
+print (dia == "sabado" or dia == "domingo")
+
+letra = input("dime una letra: ")
+print (letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u")
+
+
